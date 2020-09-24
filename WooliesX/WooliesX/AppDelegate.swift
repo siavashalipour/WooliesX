360fc5244e0672e2b52b6a2cef2d6dd8ba2db8d1
Siavash <siavash.abbasalipour@zip.co>
2020-09-24 17:23:17 +1000

    initial commit - project init

---
+++ WooliesX/WooliesX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WooliesX
+//
+//  Created by Siavash on 24/9/20.
+//  Copyright © 2020 Siavash. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ WooliesX/WooliesX/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WooliesX
+//
+//  Created by Siavash on 24/9/20.
+//  Copyright © 2020 Siavash. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
